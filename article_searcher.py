22d6ae3b7fc3fc75f8bbb217950e47b907644277
Miguel Tya <migueltya@test.com>
2020-08-20 13:38:16 +0200

    Created venv and collection of training articles

--- article_searcher.py
+++ article_searcher.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-from websites import websites
+from training_articles import training_articles
 
 def get_article(article_url, website):
     res = requests.get(article_url)
     res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text)
-    article = soup.find("div", {"id": websites[website]})
+    soup = BeautifulSoup(res.text, features="html.parser")
+    article = soup.find("div", {"id": website})
     return article
+
+print(get_article(training_articles[0][0], training_articles[0][1]))
