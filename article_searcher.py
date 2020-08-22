d08126b43948f9725b21587d91aa10b1a08ff89d
Miguel Tya <migueltya@test.com>
2020-08-22 20:08:54 +0200

    Adding jieba library to tokenize chinese articles.

--- article_searcher.py
+++ article_searcher.py
@@ -1,6 +1,10 @@
+# -*- coding:utf-8 -*-
+
 import requests
+import sys
 from bs4 import BeautifulSoup
 from websites import websites
+import jieba
 
 from training_articles import training_articles
 
@@ -10,4 +14,23 @@ def get_article(article_url, website):
     article = soup.find("div", {"id": websites[website]})
     return article.encode("utf-8")
 
-print(get_article(training_articles[0][0], training_articles[0][1]))
+def tokenizer(article):
+    seg_list = jieba.cut(article)
+    return " / ".join(seg_list).encode(sys.stdout.encoding, errors='replace')
+
+def main(training_articles):
+    article = get_article(training_articles[0][0], training_articles[0][1])
+    print(" \n\nPrinting Article")
+    print(article)
+    article_tokenized = tokenizer(article)
+    print("\n\nPrinting Tokens")
+    print(article_tokenized)
+    print(type(article_tokenized))
+    with open("test.html", 'w') as file:
+        print(sys.stdout.encoding)
+        file.write(article_tokenized.decode("utf-8", errors="ignore"))
+
+if __name__ == "__main__":
+    main(training_articles)
+
+
