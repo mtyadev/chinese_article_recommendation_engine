6f7be3a7f2421990be5ba0353fb4385f6db2f0c8
Miguel Tya <migueltya@test.com>
2020-08-22 17:35:44 +0200

    Able to retreive articles and process as utf-8.

--- article_searcher.py
+++ article_searcher.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from websites import websites
 
 from training_articles import training_articles
 
 def get_article(article_url, website):
     res = requests.get(article_url)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, features="html.parser")
-    article = soup.find("div", {"id": website})
-    return article
+    soup = BeautifulSoup(res.content, features="html.parser")
+    article = soup.find("div", {"id": websites[website]})
+    return article.encode("utf-8")
 
 print(get_article(training_articles[0][0], training_articles[0][1]))
